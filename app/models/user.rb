f6573dd42350af467442178f34ed9f1ba4ae3102
Sammy Dallal <sammydallal@gmail.com>
2016-09-13 17:51:47 -0700

    testing to see if angular works added test controller and JS file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, presence: true
-  validates :last_name, presence: true  
+  validates :last_name, presence: true 
+  validates :username, presence: true  
   validates :username, uniqueness: true    
 
 
   def full_name
     "#{first_name} #{last_name}"
   end 
-  
+
 end
