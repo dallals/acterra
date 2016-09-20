c79d1521cdfa96b6951d4f80ef3b6f6d9fd65342
GNrosen <grant.rosen1@gmail.com>
2016-09-20 10:59:26 -0700

    fifth commit

--- app/assets/javascripts/indexController.js
+++ app/assets/javascripts/indexController.js
@@ -2,7 +2,6 @@ var app = angular.module('acterra');
 
 app.controller('indexController', ["$scope", 'indexFactory', function($scope, indexFactory){
 	console.log("indexController loaded");
-	$scope.test = "hello";
 
 	indexFactory.getOrganizations(function(data){
 		$scope.organizations = data;
