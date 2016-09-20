bd6ac22ebc26a1497495a0a9ba55776abe2c2484
Nazim Bellahsene <nazim.bellahsene@gmail.com>
2016-09-20 10:36:05 -0700

    unique counties

--- app/assets/javascripts/indexController.js
+++ app/assets/javascripts/indexController.js
@@ -1,11 +1,41 @@
 var app = angular.module('acterra');
 
-app.controller('indexController', ["$scope", 'indexFactory', function($scope, indexFactory){
-	console.log("indexController loaded");
-	$scope.test = "hello";
-
+app.controller('indexController', ["$scope",'indexFactory', function($scope, indexFactory){
+	// var organizations;
 	indexFactory.getOrganizations(function(data){
 		$scope.organizations = data;
 		console.log($scope.organizations);
-	})
+
+		function unique(arr){
+			var result = [];
+			$.each(arr, function(i, e){
+				if($.inArray(e, result) == -1) result.push(e);
+			});
+			return result;
+		}
+
+		var counties = function(data){
+			var newArr=[];
+			$.each(data, function(i, e){
+				newArr.push(e.county_name);
+			})
+			return newArr;
+		}
+
+		$scope.counties = unique(counties(data));
+
+		// $scope.counties = function(data){
+		// 	var newArr = []
+		// 	for(var x in data){
+		// 		newArr.push(data[x].org_name);
+		// 	}
+		// 	console.log(newArr);
+		// 	return newArr.unique();
+		// };
+
+
+
+	});
+
+
 }]);
