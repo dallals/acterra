23c3db61b80ad5b0e12985f946ad71cf197e9460
Sammy Dallal <sammydallal@gmail.com>
2016-09-27 21:40:26 -0700

    added capitlize to users

--- app/models/award.rb
+++ app/models/award.rb
@@ -8,7 +8,7 @@ class Award < ActiveRecord::Base
 	has_many :award_years, through: :year_awards
 	# has_one  :award_year
 	validates :name, presence: true 
-	before_save { |award| award.name = organization.name.titleize }
+	before_save { |award| award.name = award.name.titleize }
   
 	# validate :only_one_year
 
