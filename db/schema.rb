1f365337ee75055e58dc719d74a1e0437bae5b4f
Sammy Dallal <sammydallal@gmail.com>
2016-10-10 12:29:42 -0700

    added organization_type table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921214842) do
+ActiveRecord::Schema.define(version: 20161010192015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,12 @@ ActiveRecord::Schema.define(version: 20160921214842) do
   add_index "org_awards", ["award_id"], name: "index_org_awards_on_award_id", using: :btree
   add_index "org_awards", ["organization_id"], name: "index_org_awards_on_organization_id", using: :btree
 
+  create_table "organization_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "organizations", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -83,9 +89,9 @@ ActiveRecord::Schema.define(version: 20160921214842) do
     t.string   "phone"
     t.string   "email"
     t.integer  "county_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "organization_type"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "organization_type_id"
   end
 
   add_index "organizations", ["county_id"], name: "index_organizations_on_county_id", using: :btree
@@ -116,11 +122,15 @@ ActiveRecord::Schema.define(version: 20160921214842) do
   create_table "year_awards", force: :cascade do |t|
     t.integer  "award_id"
     t.integer  "award_year_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer  "organization_id"
+    t.integer  "county_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "year_awards", ["award_id"], name: "index_year_awards_on_award_id", using: :btree
   add_index "year_awards", ["award_year_id"], name: "index_year_awards_on_award_year_id", using: :btree
+  add_index "year_awards", ["county_id"], name: "index_year_awards_on_county_id", using: :btree
+  add_index "year_awards", ["organization_id"], name: "index_year_awards_on_organization_id", using: :btree
 
 end
